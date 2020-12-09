217edf35abb1f6ebb3ab29ae2980a15f34bdcfea
Vivien-Ploix <vivien.ploix@gmail.com>
2020-12-09 14:41:01 +0100

    add persist store, to test

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
@@ -8,31 +8,35 @@ import Login from "./Pages/Login";
 import SignUp from "./Pages/Signup";
 import GameInfos from "./Pages/GameInfos"
 import { Provider } from "react-redux";
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const App = () => {
+
   return (
     <Provider store={store}>
-      <div>
-        <Router>
-          <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/sign-up">
-              <SignUp />
-            </Route>
-            <Route path="/gameInfos">
-              <GameInfos />
-            </Route>
-          </Switch>
-          <Footer />
-        </Router>
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div>
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/sign-up">
+                <SignUp />
+              </Route>
+              <Route path="/gameInfos">
+                <GameInfos />
+              </Route>
+            </Switch>
+            <Footer />
+          </Router>
+        </div>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import authReducer from './authentication/authReducer';
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, authReducer)
 
 let store = createStore(
-    authReducer,
+    persistedReducer,
     compose(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
+let persistor = persistStore(store)
 
-export default store;
+export {store, persistor};
