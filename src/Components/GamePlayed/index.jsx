d3e321c7e4f7cce15438a2a15bda391aff011226
Vivien-Ploix <vivien.ploix@gmail.com>
2020-12-10 10:05:41 +0100

    add gamePlayedList, containing map on gamesPlayed

--- src/Components/GamePlayed/index.jsx
+++ src/Components/GamePlayed/index.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
 
-const GamePlayed = () => {
+const GamePlayed = ({ gameId, opponentId }) => {
+  const [opponent, setOpponent] = useState({});
+  const getOpponentInfos = () => {
+    fetch(`https://pyramid-race-api.herokuapp.com/users/${opponentId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOpponent(data);
+      })
+      .catch((error) => alert(error));
+  };
+
+  useEffect(() => {
+    getOpponentInfos();
+  }, []);
+
   return (
     <div class="container h-100">
       <div class="row align-middle">
-        <div class="col-md-6 col-lg-4 column">
+        <div class="col-md-8 col-lg-6 column">
           <div class="card gr-1">
             <div class="txt">
               <h1>Défaite</h1>
               <p>Vous avez atteint l'étage 3 de la pyramide</p>
-              <small>Adversaire : Jean</small>
+              <small>Adversaire : {opponent.pseudo}</small>
             </div>
             <a href="#">Rejouer</a>
             <div class="ico-card">
--- src/Pages/GameInfos/index.jsx
+++ src/Pages/GameInfos/index.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import GamePlayed from "../../Components/GamePlayed";
-import JumbotronGameInfos from '../../Components/JumbotronGameInfos'
-import PlayerInfos from '../../Components/PlayerInfos'
-import FriendList from '../../Components/FriendList'
-import './style.scss'
+import JumbotronGameInfos from "../../Components/JumbotronGameInfos";
+import PlayerInfos from "../../Components/PlayerInfos";
+import FriendList from "../../Components/FriendList";
+import "./style.scss";
+import GamePlayedList from "../../Components/GamePlayedList";
 
 const GameInfos = () => {
   return (
     <div>
-      <JumbotronGameInfos/>
-      <div className= 'GameInfoContainer'>
-      <GamePlayed />
-      <PlayerInfos/>
-      <FriendList/>
+      <JumbotronGameInfos />
+      <div className="GameInfoContainer">
+        <GamePlayedList />
+        <PlayerInfos />
+        <FriendList />
       </div>
-      
     </div>
   );
 };
