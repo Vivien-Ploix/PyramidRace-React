cd99774d4291a2ccce1086d19cceebf7ff758a9b
root <root@Yoni-PC.localdomain>
2020-12-14 19:10:01 +0100

    infoPageFix history

--- src/Pages/GameInfos/GamePlayedList/index.jsx
+++ src/Pages/GameInfos/GamePlayedList/index.jsx
@@ -7,6 +7,7 @@ import "./style.scss";
 const GamePlayedList = () => {
   const userId = useSelector((state) => state.id);
   const [gamesPlayed, setGamesPlayed] = useState([]);
+  const [gamesToPlay, setGamesToPlay] = useState([]);
   const [playerStats, setPlayerStats] = useState({});
   const [gamesPlayedStats, setGamesPlayedStats] = useState([]);
 
@@ -21,8 +22,22 @@ const GamePlayedList = () => {
       .catch((error) => console.log(error));
   };
 
+  const fetchGamesToPlay = () => {
+    fetch(
+      `https://pyramid-race-api.herokuapp.com/users/${userId}/games_to_play`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setGamesPlayedStats(data);
+        let slicedData = data.slice(0, 10);
+        setGamesToPlay(slicedData);
+      })
+      .catch((error) => console.log(error));
+  };
+
   useEffect(() => {
     fetchGamesPlayed();
+    fetchGamesToPlay();
   }, []);
 
   useEffect(() => {
@@ -35,53 +50,91 @@ const GamePlayedList = () => {
       ).length,
     };
     setPlayerStats(player_stats);
+    console.log(gamesPlayed);
   }, [gamesPlayed]);
 
   return (
     <div className="GameInfoContainer">
       <div className="game-info-column col-lg-8">
-      <h1 className="text-center" >Parties en cours</h1>
-      <br/>
-      <div className="list-container">
-       
-       {gamesPlayed.map((game) => {
-         if (game.winner_id === null) {
-           return (
-             <GamePlayed
-               gameId = {game.id}
-               opponentId = {game.player2_id}
-               winner_id = {game.winner_id}
-               key = {game.id}
-             />
-           )}})}
-           </div>
-
-      <h1 className="text-center" >Parties terminées</h1>
-      <br/>
+        <h1 className="text-center">Parties en cours</h1>
+        <br />
         <div className="list-container">
-       
+          {gamesToPlay.map((game) => {
+            if (game.winner_id === null && game.player1_id == userId) {
+              return (
+                <GamePlayed
+                  gameId={game.id}
+                  opponentId={game.player2_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
+                />
+              );
+            } else if (game.winner_id === null && game.player2_id == userId) {
+              return (
+                <GamePlayed
+                  gameId={game.id}
+                  opponentId={game.player1_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
+                />
+              );
+            }
+          })}
+        </div>
+
+        <h1 className="text-center">Parties terminées</h1>
+        <br />
+        <div className="list-container">
           {gamesPlayed.map((game) => {
-            if (game.winner_id === userId) {
+            if (game.winner_id == userId && game.player1_id == userId) {
               return (
                 <GamePlayed
-                  gameId = {game.id}
-                  opponentId = {game.player2_id}
-                  winner_id = {game.winner_id}
-                  key = {game.id}
+                  gameId={game.id}
+                  opponentId={game.player2_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
                 />
               );
-            } else if (!!game.winner_id && game.winner_id !== userId ){
+            } else if (
+              game.winner_id == userId &&
+              game.player2_id == userId
+            ) {
               return (
                 <GamePlayed
-                  gameId = {game.id}
-                  opponentId = {game.player1_id}
-                  winner_id = {game.winner_id}
-                  key = {game.id}
+                  gameId={game.id}
+                  opponentId={game.player1_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
+                />
+              );
+            } else if (
+              game.winner_id &&
+              game.winner_id !== userId &&
+              game.player2_id == userId
+            ) {
+              return (
+                <GamePlayed
+                  gameId={game.id}
+                  opponentId={game.player1_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
+                />
+              );
+            } else if (
+              game.winner_id &&
+              game.winner_id !== userId &&
+              game.player1_id == userId
+            ) {
+              return (
+                <GamePlayed
+                  gameId={game.id}
+                  opponentId={game.player2_id}
+                  winner_id={game.winner_id}
+                  key={game.id}
                 />
               );
             }
-            }
-          )}
+          })}
         </div>
       </div>
       <div className="game-info-column col-lg-3 player-infos-col">
